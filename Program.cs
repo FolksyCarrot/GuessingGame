a9c54496541b5ee19f19ca3e2add4ea9b5f04d3a
Keith Parks <keithparks20@gmail.com>
2022-03-28 18:43:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+Main();
+
+void Main() {
+    Guesses();
+};
+
+void GuessTheNumber() {
+    int secretNumber = 42;
+    Console.WriteLine("Guess a number: ");
+    int answer = Convert.ToInt32(Console.ReadLine()); 
+    if(answer == secretNumber ) {
+        Console.WriteLine("Success");
+    } else {
+        Console.WriteLine("Keep Guessing");
+    }
+};
+
+
+void Guesses() {
+    int guess = 0;
+    while ( guess < 4) {
+        guess ++;
+        GuessTheNumber();
+    }
+    if (guess == 4) {
+        Console.WriteLine("All out of guesses");
+    }
+};
